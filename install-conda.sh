b13abe02e58e6af8c49998e226e31175645a4168
miraculixx <miraculixx@gmx.ch>
2017-03-12 01:03:02 +0100

    add shippable build

--- install-conda.sh
+++ install-conda.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CONDA_DIR=$HOME/.omegaml/miniconda
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 chmod +x Miniconda2-latest-Linux-x86_64.sh
 ./Miniconda2-latest-Linux-x86_64.sh -b -p $CONDA_DIR
 echo "PATH=$CONDA_DIR:\$PATH" >> $HOME/.bashrc
-export PATH=$CONDA_DIR:$PATH
-conda install --file conda-requirements.txt
+export PATH=$CONDA_DIR/bin:$PATH
+conda install -y -q --file conda-requirements.txt 
+pip install -r pip-requirements.txt
 conda env list
 
