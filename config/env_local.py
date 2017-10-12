f36a9d4267deff8d8dba3bb14d4a2a980d4e70aa
miraculixx <miraculixx@gmx.ch>
2017-10-12 23:07:07 +0200

    update

--- config/env_local.py
+++ config/env_local.py
@@ -1,16 +1,23 @@
+from stackable.contrib.config.conf_allauth import Config_DjangoAllAuth
+from stackable.contrib.config.conf_cities_light import Config_Cities_Light
+from stackable.contrib.config.conf_payment import Config_DjangoPayments
+from stackable.contrib.config.conf_sekizai import Config_DjangoSekizai
+from stackable.contrib.config.email.filebased import Config_FileEmail
+from stackable.contrib.config.conf_djangoadmin import Config_DjangoAdmin
+from stackable.contrib.config.conf_bootstrap import Config_Bootstrap3
+
 from stackable.stackable import StackableSettings
 
 from config.env_global import EnvSettingsGlobal
-from stackable.contrib.config.conf_payment import Config_DjangoPayments
-from stackable.contrib.config.conf_cities_light import Config_Cities_Light
-from stackable.contrib.config.conf_allauth import Config_DjangoAllAuth
-from stackable.contrib.config.email.filebased import Config_FileEmail
 
 
-class EnvSettings_Local(Config_DjangoPayments,
+class EnvSettings_Local(Config_DjangoSekizai,
+                        Config_Bootstrap3,
+                        Config_DjangoPayments,
                         Config_FileEmail,
                         Config_Cities_Light,
                         Config_DjangoAllAuth,
+                        Config_DjangoAdmin,
                         EnvSettingsGlobal):
     _addl_apps = ('omegaweb',
                   'tastypie_swagger',
