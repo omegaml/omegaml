4c61cf8e1ab6d2cc084931b1f26547a0a4bfa8dc
miraculixx <miraculixx@gmx.ch>
2018-01-22 23:51:10 +0100

    buildpack fix

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+# conda buildpack dependency install
+# from https://github.com/arose13/conda-buildpack/blob/e9a1f10f84dcc85d8eb8ac2d034817cbfafc77c6/bin/steps/conda_compile
+
+if [ -f conda-requirements.txt ]; then
+    puts-step "Installing dependencies using Conda"
+    conda install --file conda-requirements.txt --yes | indent
+fi
+
+if [ -f requirements.txt ]; then
+    puts-step "Installing dependencies using Pip"
+    pip install -r requirements.txt  --exists-action=w --allow-all-external | indent
+fi
