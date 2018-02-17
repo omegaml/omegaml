a0143d5e2a9f939c9019284e67f79eb5a38eec79
miraculixx <miraculixx@gmx.ch>
2018-02-17 11:16:47 +0100

    fix release builds

--- scripts/distrelease.sh
+++ scripts/distrelease.sh
@@ -4,6 +4,10 @@
 ## Build a full distribution release including dependencies
 ##    @script.name [option]
 ##
+## Options:
+##    --version=VALUE the version to build. defaults to 0.1
+##
+##
 
 # script setup to parse options
 script_dir=$(dirname "$0")
@@ -16,10 +20,15 @@ sourcedir=$script_dir/..
 sourcedir=$(realpath $sourcedir)
 distdir=$script_dir/../dist
 distdir=$(realpath $distdir)
+version=${version:=0.1}
+releasezip=$distdir/omegaml-release-$version.zip
 
 # prepare
+mkdir -p $distdir
 rm -rf $distdir/*.zip
+rm -rf $distdir/*.whl
 rm -rf $distdir/build
+rm -rf $distdir/docker-stgaging
 
 # build
 $release --source ../omegaml
@@ -34,28 +43,31 @@ unzip landingpage.zip "*whl"
 unzip stackable.zip "*whl"
 unzip django-tastypie-swagger.zip "*whl"
 unzip ccbackend.zip "*whl"
-zip omegaml.zip *whl
+unzip omegaml.zip "*whl"
+zip $releasezip *whl
 popd 
 
 # add requirements and stuff
 pushd $distdir
-cp $sourcedir/conda-requirements.txt $distdir
-cp $sourcedir/requirements.txt $distdir
-cp $sourcedir/Procfile $distdir
-cp $sourcedir/README.rst $distdir
-cp $sourcedir/manage.py $distdir
-zip omegaml.zip conda-requirements.txt requirements.txt Procfile README.rst manage.py
+cp $sourcedir/conda-requirements.txt .
+cp $sourcedir/pip-requirements.txt ./requirements.txt
+cp $sourcedir/Procfile .
+cp $sourcedir/README.rst .
+cp $sourcedir/manage.py .
+zip $releasezip conda-requirements.txt requirements.txt Procfile README.rst manage.py
 popd
 
 # add distribution files
 pushd $distdir
-zip -j omegaml.zip $sourcedir/release/dist/* 
+zip -j $releasezip $sourcedir/release/dist/* 
 popd
 
-# build docker image
-pushd $distdir
-unzip omegaml.zip -d build
+# build docker image from release zip
+mkdir -p $distdir/docker-staging
+pushd $distdir/docker-staging
+unzip $releasezip -d build
 pushd build
+docker images | grep omegaml | xargs | cut -f 3 -d ' ' | xargs docker rmi --force
 docker build -f Dockerfile -t omegaml .
 popd
 popd
