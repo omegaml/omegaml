ba07e9a69ae4511bbcd038757b4cea4e1e43e4e7
miraculixx <miraculixx@gmx.ch>
2018-03-30 00:06:50 +0200

    add mailgun configuration

--- config/env_omegamlio.py
+++ config/env_omegamlio.py
@@ -1,3 +1,4 @@
+from stackable import StackableSettings
 from stackable.contrib.config.conf_dokku import Config_Dokku
 
 from .env_local import EnvSettings_Local
@@ -17,3 +18,16 @@ class EnvSettings_omegamlio(Config_Dokku,
                        'rabbitmq broker url'),
         'CELERY_ALWAYS_EAGER': (False, 'if True celery tasks are processed locally'),
     }
+
+    # mail gun email settings
+    # see https://app.mailgun.com/app/domains/mg.omegaml.io
+    ANYMAIL = {
+        # (exact settings here depend on your ESP...)
+        "MAILGUN_API_KEY": "key-2u-chky4r83ljbhe522bkmegwixvij46",
+        "MAILGUN_SENDER_DOMAIN": 'mg.omegaml.io',  # your Mailgun domain, if needed
+    }
+    EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or...
+    DEFAULT_FROM_EMAIL = "admin@omegaml.io"  # if you don't already have this in settings
+
+    _addl_apps = ('anymail',)
+    StackableSettings.patch_apps(_addl_apps)
