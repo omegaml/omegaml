694048ca1b8bc319d2d3a48fb09332d21faa5a0d
miraculixx <miraculixx@gmx.ch>
2018-03-29 08:39:55 +0200

    update api user identification

--- omegaweb/static/omegaweb/js/settings.js
+++ omegaweb/static/omegaweb/js/settings.js
@@ -1,11 +1,9 @@
 define(['backbone'], function($B) {
-    console.log('cleverpendeln settings initialized');
+    console.log('omegaml settings initialized');
     var settings = {
         debug : true,
         authApiUri : '/api/v2/auth',
         apiUrl : '/api/v1',
     };
-    $B.Tastypie.apiKey.username = 'test5';
-    $B.Tastypie.apiKey.key = 'ba6a6d4bcc0af89e643ae64562e1dcd340f67100';
     return settings;
 });
