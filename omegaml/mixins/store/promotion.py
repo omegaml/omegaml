b20e8c663413817d7548340641cc4bc3c20a4ae3
miraculixx <miraculixx@users.noreply.github.com>
2020-08-20 00:15:13 +0200

    enable promotion of versioned models to work as expected (#109)
    also some small cleanups

--- omegaml/mixins/store/promotion.py
+++ omegaml/mixins/store/promotion.py
@@ -6,7 +6,7 @@ class PromotionMixin(object):
     Promote objects from one bucket to another
     """
 
-    def promote(self, name, other):
+    def promote(self, name, other, asname=None, **kwargs):
         """
         Promote object to another bucket.
 
@@ -15,8 +15,9 @@ class PromotionMixin(object):
 
         Args:
             name: The name of the object
-            bucket: the bucket to promote to
-            other:
+            other: the OmegaStore instance to promote to
+            asname: the name to use in other, defaults to .metadata(name).name
+            kwargs: will be forwarded to other.put
 
         Returns:
             The Metadata of the new object
@@ -26,17 +27,18 @@ class PromotionMixin(object):
         # see if the backend supports explicit promotion
         backend = self.get_backend(name)
         if hasattr(backend, 'promote'):
-            return backend.promote(name, other)
+            return backend.promote(name, other, asname=asname, **kwargs)
         # do default promotion, i.e. copy
         meta = self.metadata(name)
         obj = self.get(name)
-        other.drop(name, force=True)
+        asname = asname or meta.name
+        other.drop(asname, force=True)
         # TODO refactor to promote of python native data backend
         if meta.kind == MDREGISTRY.PYTHON_DATA:
             # in case of native python data we get back a list of
             # all previously inserted objects. do the same in other
-            [other.put(o, name) for o in obj]
-            other_meta = other.metadata(name)
+            [other.put(o, asname) for o in obj]
+            other_meta = other.metadata(asname)
         else:
-            other_meta = other.put(obj, name)
+            other_meta = other.put(obj, asname, **kwargs)
         return other_meta
