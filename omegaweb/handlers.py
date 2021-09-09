edb67480a6fbe2da003a8b87ad8fa43c5b6b78d7
miraculixx <miraculixx@users.noreply.github.com>
2021-09-09 21:10:12 +0200

    ensure sensible usernames

--- omegaweb/handlers.py
+++ omegaweb/handlers.py
@@ -1,7 +1,19 @@
+import re
+
+import string
 from allauth.account.signals import user_signed_up
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
 from django.dispatch.dispatcher import receiver
 
 from omegaops.tasks import deploy_user_service
+
+
+@receiver(pre_save, sender=User)
+def handle_username(sender, user, **kwargs):
+    # do not allow special characters in username
+    if set(string.punctuation) & set(user.username):
+        user.username = re.sub(f'[{string.punctuation}]', '', user.username)
 
 
 @receiver(user_signed_up)
@@ -10,4 +22,3 @@ def handle_usersignup(sender, request=None, user=None, **kwargs):
     handle user sign up
     """
     deploy_user_service(user.pk)
-
