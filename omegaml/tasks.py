776cd7d5b0e84adf2733f0a1b54a92e2b519e294
miraculixx <miraculixx@users.noreply.github.com>
2022-08-02 14:33:20 +0200

    add pre-/post task hooks using common .perform() backend method
    - backend.perform('method') calls backend.method() wrapped by
    pre_/post_method calls to allow custom processing by mixins
    - backends can override _call_handler to delegate processing
    - prepares for openapi signature checks, data mapping

--- omegaml/tasks.py
+++ omegaml/tasks.py
@@ -25,65 +25,64 @@ if os.path.basename(sys.argv[0]) == 'celery':
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_predict(self, modelname, Xname, rName=None, pure_python=True, **kwargs):
-    result = self.get_delegate(modelname).predict(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('predict', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_reduce(self, results, modelName=None, rName=None, pure_python=True, **kwargs):
-    result = self.get_delegate(modelName).reduce(modelName, results, **self.delegate_kwargs)
+    result = self.get_delegate(modelName).perform('reduce', modelName, results, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
-def omega_predict_proba(self, modelname, Xname, rName=None, pure_python=True,
-                        **kwargs):
-    result = self.get_delegate(modelname).predict_proba(*self.delegate_args, **self.delegate_kwargs)
+def omega_predict_proba(self, modelname, Xname, rName=None, pure_python=True, **kwargs):
+    result = self.get_delegate(modelname).perform('predict_proba', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_fit(self, modelname, Xname, Yname=None, pure_python=True, **kwargs):
-    result = self.get_delegate(modelname).fit(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('fit', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_partial_fit(self,
                       modelname, Xname, Yname=None, pure_python=True, **kwargs):
-    result = self.get_delegate(modelname).partial_fit(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('partial_fit', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_score(self, modelname, Xname, Yname=None, rName=True, pure_python=True,
                 **kwargs):
-    result = self.get_delegate(modelname).score(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('score', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_fit_transform(self, modelname, Xname, Yname=None, rName=None,
                         pure_python=True, **kwargs):
-    result = self.get_delegate(modelname).fit_transform(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('fit_transform', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_transform(self, modelname, Xname, rName=None, **kwargs):
-    result = self.get_delegate(modelname).transform(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('transform', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
 def omega_decision_function(self, modelname, Xname, rName=None, **kwargs):
-    result = self.get_delegate(modelname).decision_function(*self.delegate_args, **self.delegate_kwargs)
+    result = self.get_delegate(modelname).perform('decision_function', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
 @shared_task(base=OmegamlTask, bind=True)
-def omega_gridsearch(self, modelname, Xname, Yname, parameters=None, **kwargs):
-    result = self.get_delegate(modelname).gridsearch(*self.delegate_args, **self.delegate_kwargs)
+def omega_gridsearch(self, modelname, Xname, Yname=None, parameters=None, **kwargs):
+    result = self.get_delegate(modelname).perform('gridsearch', *self.delegate_args, **self.delegate_kwargs)
     return sanitized(result)
 
 
@@ -126,6 +125,7 @@ def omega_ping(task, *args, logging=False, **kwargs):
         package_logger.log(pylevel, f'package log: running ping task {data}')
         print(f"print log: running ping task {data}")
     return data
+
 
 @worker_process_init.connect
 def fix_multiprocessing(**kwargs):
