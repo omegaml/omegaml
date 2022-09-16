fbb733523def29dc53bdd47624f6a4d42032b2e4
miraculixx <miraculixx@users.noreply.github.com>
2022-09-16 17:41:09 +0200

    disable remote mlflow project testing due to issue with conda envs
    - mlflow and conda do not play well
    - specifically, mlflow is not found, even though it is installed
    - disabling mlflow projects until further notice

--- omegaml/defaults.py
+++ omegaml/defaults.py
@@ -11,11 +11,11 @@ from pathlib import Path
 from omegaml.util import tensorflow_available, keras_available, module_available, markup, dict_merge
 
 # determine how we're run
-is_cli_run = os.path.basename(sys.argv[0]) == 'om'
-is_test_run = (os.environ.get('OMEGA_TEST_MODE') or
-               any('test' in basename(v) for v in sys.argv[:3]))
 truefalse = lambda v: (v if isinstance(v, bool) else
                        any(str(v).lower().startswith(c) for c in ('y', 't', '1')))
+is_cli_run = os.path.basename(sys.argv[0]) == 'om'
+is_test_run = truefalse(os.environ.get('OMEGA_TEST_MODE') or
+                        any('test' in basename(v) for v in sys.argv[:3]))
 
 #: configuration file, by default will be searched in current directory, user config or site config
 OMEGA_CONFIG_FILE = os.environ.get('OMEGA_CONFIG_FILE') or 'config.yml'
