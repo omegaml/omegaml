7698873f5374696dd79c706618337f7927e6f7a7
miraculixx <miraculixx@users.noreply.github.com>
2022-10-27 01:31:08 +0200

    upgrade dependencies

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
         'croniter>=0.3.30',
         'nbformat>=4.0.1',
         'nbconvert>=6.4.0',
-        'dill>=0.3.2',
+        'dill>=0.3.2,<0.3.6', # due to dill, https://github.com/uqfoundation/dill/issues/332
         'callable-pip>=1.0.0',
         'appdirs>=1.4.3',
         'cron-descriptor>=1.2.31',
