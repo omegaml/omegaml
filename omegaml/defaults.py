210da5b3cb1aa37d0a40241b274fdceea5828423
miraculixx <miraculixx@users.noreply.github.com>
2024-06-26 14:18:57 +0200

    improve loading spee

--- omegaml/defaults.py
+++ omegaml/defaults.py
@@ -18,6 +18,9 @@ is_cli_run = os.path.basename(sys.argv[0]) == 'om'
 is_test_run = truefalse(os.environ.get('OMEGA_TEST_MODE'))
 is_test_run |= len(set(test_runners) & set(cmd_args)) and 'omegaml-ce' in str(Path().cwd())
 
+# enable unicode emoijs in stdout
+sys.stdout.reconfigure(encoding='utf-8')
+
 #: configuration file, by default will be searched in current directory, user config or site config
 OMEGA_CONFIG_FILE = os.environ.get('OMEGA_CONFIG_FILE') or 'config.yml'
 #: the temp directory used by omegaml processes
