5b85e95ce31823e440ecf6651928ffdf9c7a4db3
miraculixx <miraculixx@users.noreply.github.com>
2024-09-11 20:52:12 +0200

    docs: build origin/master instead of local master

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -218,13 +218,13 @@ smv_tag_whitelist = r'^latest|stable|(release/[02]\.\d+(.\d+)?$)|([02]\.\d+(.\d+
 # Whitelist pattern for branches (set to None to ignore all branches)
 smv_branch_whitelist = r'master'
 # Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = None
+smv_remote_whitelist = 'origin'
 # Pattern for released versions
 smv_released_pattern = r'(refs/)?tags/release/.*'
 # Format for versioned output directories inside the build directory
 smv_outputdir_format = '{ref.name}'
 # Determines whether remote or local git branches/tags are preferred if their output dirs conflict
-smv_prefer_remote_refs = False
+smv_prefer_remote_refs = True
 # latest version
 smv_latest_version = f'release/{version}'
 
