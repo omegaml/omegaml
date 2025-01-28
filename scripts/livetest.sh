421d44fbb58d8318045cabfd88b0e5c3ab02d946
miraculixx <miraculixx@users.noreply.github.com>
2025-01-28 22:57:49 +0100

    fix: release test issues
    - update to latest yfinance, jupyter lab

--- scripts/livetest.sh
+++ scripts/livetest.sh
@@ -13,7 +13,6 @@
 ##      --tags=VALUE      if specified execute this behave tag only
 ##      --pyver=VALUE     the python version for the livetest image, defaults to current python version
 ##      --debug           if specified drops into pdb on error
-##      --headless        if specified runs chrome headless
 ##
 # script setup to parse options
 script_dir=$(dirname "$0")
@@ -94,7 +93,7 @@ fi
 echo "Running selenium grid locally"
 docker run -d -it -p 4444:4444 -p 7900:7900 $docker_network $docker_resources -e SE_START_VNC=false --name selenium --network-alias selenium selenium/standalone-chrome
 countdown 10
-echo "Running the livetest image using port: $chrome_debug_port resources: $docker_resources network: $docker_network image: $docker_image env: $docker_env features: $behave_features $LIVETEST_BEHAVE_EXTRA_OPTS"
+echo "Running the livetest image using port: $chrome_debug_port resources: $docker_resources network: $docker_network image: $docker_image env: $docker_env features: $behave_features $LIVETEST_BEHAVE_EXTRA_OPTS" debug: $BEHAVE_DEBUG
 mkdir -p ~/.omegaml
 docker run -it -p $chrome_debug_port $BEHAVE_DEBUG -e CHROME_HEADLESS=1 -e CHROME_SCREENSHOTS=/tmp/screenshots -v ~/.omegaml:/root/.omegaml -v /tmp/screenshots:/tmp/screenshots $docker_resources $docker_network $docker_env $docker_image behave --no-capture $behave_features $LIVETEST_BEHAVE_EXTRA_OPTS $behave_options
 success=$?
