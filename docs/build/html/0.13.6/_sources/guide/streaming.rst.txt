9ed16f5a1036fe3638498f47dcab394e642e4f31
miraculixx <miraculixx@users.noreply.github.com>
2025-04-03 19:59:44 +0200

    docs: updates ghpages

---
+++ docs/build/html/0.13.6/_sources/guide/streaming.rst.txt
@@ -0,0 +1,4 @@
+Mini-Batch Streaming
+====================
+
+See minibatch documentation at https://github.com/omegaml/minibatch
--- tutorial/omegaml-tutorial/mnist-app/mnist/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# create_app must be importable from the package
-# -- i.e. apphub will do this:
-#    import mnist
-#    server = mnist.create_app()
-from .app import create_app
