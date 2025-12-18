bfeb958bace5c243b5a25bf3de929f5b88db90dd
miraculixx <miraculixx@users.noreply.github.com>
2025-12-19 00:21:45 +0100

    update multiversion

--- docs/scripts/marknext.sh
+++ docs/scripts/marknext.sh
@@ -11,13 +11,14 @@
 ##
 script_dir=$(realpath "$(dirname "$0")")
 source $script_dir/easyoptions || exit
-src_dir=$script_dir/../../omegaml
+src_dir=$script_dir/../..
 # Read the version string once
 # -- read basic version, including any -modifiers, e.g. 1.2.3-rc1
-VERSION=$(head -n1 $src_dir/VERSION)
+VERSION=$(head -n1 $src_dir/omegaml/VERSION)
 # -- remove any modifiers, e.g. 1.2.3-rc1 => 1.2.3
 VERSION=${VERSION%%-*} 
 # Replace only whole‑word occurrences of “ NEXT ” in every *.rst file
 echo "INFO changing > NEXT < to > $VERSION <... (may take a few minutes)"
-find $src_dir -type f -name '*.rst' -o -name '*.py' | xargs sed -i "s/\bNEXT\b?/${VERSION}/g"
+find $src_dir/omegaml -type f -name '*.rst' -o -name '*.py' | xargs sed -i "s/\bNEXT\b?/${VERSION}/g"
+find $src_dir/docs/source -type f -name '*.rst' -o -name '*.py' | xargs sed -i "s/\bNEXT\b?/${VERSION}/g"
 echo "INFO Done."
